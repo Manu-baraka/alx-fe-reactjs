13baeb45596cc1b1cbcad5080f4d2c587befa8ee
Emmanuel <emmanuelbaraka572@gmail.com>
2026-01-18 21:02:08 +0300

    Modified UserProfile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,12 @@
-const UserProfile = (props) => {
-   return (
-     <div>
-       <h2>{props.name}</h2>
-       <p>Age: {props.age}</p>
-       <p>Bio: {props.bio}</p>
-     </div>
-   );
- };
- 
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>{userData.name}</h2>
+      <p>{userData.email}</p>
+    </div>
+  );
+}
+
 export default UserProfile;
