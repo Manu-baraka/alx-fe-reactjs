20e0397a156baf8a32d0ef2e06612a0ed2a565d1
Emmanuel <emmanuelbaraka572@gmail.com>
2026-01-24 23:52:10 +0300

    Added a new directory

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
-import  AddRecipeForm  from './components/AddRecipeForm.jsx'
-import  RecipeList  from './components/RecipeList.jsx'
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
 
 function App() {
   return (
