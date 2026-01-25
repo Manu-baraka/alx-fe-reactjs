b7309669a488905f8dd7e9fd30b7474f055f1ce3
Emmanuel <emmanuelbaraka572@gmail.com>
2026-01-25 17:54:24 +0300

    Added new Recommendations.jsx and FavoritesList.jsx

---
+++ recipe-sharing-app/src/components/RecommendationsList.jsx
@@ -0,0 +1,25 @@
+import { useRecipeStore } from "./recipeStore";
+
+const RecommendationsList = () => {
+  const recommendations = useRecipeStore(state => state.recommendations);
+
+  return (
+    <div>
+      <h2>Recommendations</h2>
+      {recommendations.map(recipe => (
+        <div
+          key={recipe.id}
+          style={{
+            width: "100%",
+            padding: "10px",
+            fontSize: "16px",
+            marginBottom: "20px",
+          }}
+        >
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
