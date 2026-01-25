3fd09b93f85e664d320a8f013deab6b757afe609
Emmanuel <emmanuelbaraka572@gmail.com>
2026-01-25 17:38:22 +0300

    Modified RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,6 @@
 // RecipeList component
   import { useRecipeStore } from '..store/recipeStore';
+import { Link } from 'react-router-dom';
 
   const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
@@ -22,6 +23,7 @@
           <div key={recipe.id}>
             <h3>{recipe.title}</h3>
             <p>{recipe.description}</p>
+            <Link to={`/recipes/${recipe.id}`}>View Details</Link>
           </div>
         ))}
       </div>
