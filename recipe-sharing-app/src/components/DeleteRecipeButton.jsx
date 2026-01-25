028477c1cdf00584451ef252c643a123c0318227
Emmanuel <emmanuelbaraka572@gmail.com>
2026-01-25 16:35:52 +0300

    Added new directory

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,10 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+  const navigate = useNavigate();
 
   return (
-    <button onClick={() => deleteRecipe(recipeId)}>
+    <button onClick={() => {
+      deleteRecipe(recipeId);
+      navigate('/');
+    }}>
       Delete Recipe
     </button>
   );
